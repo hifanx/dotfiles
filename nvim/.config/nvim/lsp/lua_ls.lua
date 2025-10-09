cacc5d4c64c1e3d15996bb76ba01cbf14488ca0f
hifanx <373441770@qq.com>
2025-10-10 03:02:00 +0800

    chore(nvim): remove legacy config

--- nvim/.config/nvim/lsp/lua_ls.lua
+++ nvim/.config/nvim/lsp/lua_ls.lua
@@ -24,21 +24,12 @@ return {
         globals = { "vim", "spec" },
         disable = { "missing-fields" },
       },
-      runtime = {
-        version = "LuaJIT",
-        special = {
-          spec = "require",
-        },
-      },
       workspace = {
         checkThirdParty = false,
         library = {
           [vim.fn.expand "$VIMRUNTIME/lua"] = true,
           [vim.fn.stdpath "config" .. "/lua"] = true,
         },
-      },
-      telemetry = {
-        enable = false,
       },
     },
   },
