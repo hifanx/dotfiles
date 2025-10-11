7b0cc08313a871f63ca0008d4f40d917950dbf85
hifanx <373441770@qq.com>
2025-10-11 21:56:24 +0800

    chore(nvim): remove keymap I don't need

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -78,24 +78,6 @@ return {
     { "<Leader>gh", function() Snacks.lazygit.log_file() end, desc = "Lazy[g]it Current File [H]istory" },
     { "<Leader>gg", function() Snacks.lazygit() end, desc = "Lazy[g]it" },
     { "<Leader>gl", function() Snacks.lazygit.log() end, desc = "Lazy[g]it [L]og" },
-    {
-      "<leader>N",
-      desc = "Neovim [N]ews",
-      function()
-        Snacks.win {
-          file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
-          width = 0.6,
-          height = 0.6,
-          wo = {
-            spell = false,
-            wrap = false,
-            signcolumn = "yes",
-            statuscolumn = " ",
-            conceallevel = 3,
-          },
-        }
-      end,
-    },
   },
   opts = {
     bigfile = { enabled = true },
