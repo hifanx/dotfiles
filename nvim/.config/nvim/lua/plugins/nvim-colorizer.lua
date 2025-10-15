bb2309cbee5d1d7b52c0cd406f467deaa7c4d598
hifanx <373441770@qq.com>
2025-10-16 02:39:55 +0800

    feat(nvim): deprecate nvchad's colorizer

--- nvim/.config/nvim/lua/plugins/nvim-colorizer.lua
+++ nvim/.config/nvim/lua/plugins/nvim-colorizer.lua
@@ -1,5 +1,16 @@
 return {
-  'NvChad/nvim-colorizer.lua',
+  'catgoose/nvim-colorizer.lua',
   event = { 'BufReadPre', 'BufNewFile' },
-  config = function() require('colorizer').setup() end,
+  config = function()
+    require('colorizer').setup {
+      user_default_options = {
+        RRGGBBAA = true,
+        AARRGGBB = true,
+        css = true,
+        xterm = true,
+        mode = 'virtualtext',
+        virtualtext = ' ',
+      },
+    }
+  end,
 }
