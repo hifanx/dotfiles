679f23362b6d1f29a6984bb6fbe19788c92be0e2
hifanx <373441770@qq.com>
2025-10-16 14:42:22 +0800

    feat(nvim): enable native extui, bye bye press-and-enter
    natively supported since neovim 0.12, now :messages will be directed to
    an interactive buffer at cmd or box, depending on what's been set.

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -60,3 +60,13 @@ g.loaded_node_provider = 0
 g.loaded_python3_provider = 0
 g.loaded_perl_provider = 0
 g.loaded_ruby_provider = 0
+
+require('vim._extui').enable {
+  enable = true, -- Whether to enable or disable the UI.
+  msg = { -- Options related to the message module.
+    ---@type 'cmd'|'msg' Where to place regular messages, either in the
+    ---cmdline or in a separate ephemeral message window.
+    target = 'cmd',
+    timeout = 4000, -- Time a message is visible in the message window.
+  },
+}
