36c76d195c9c1c6165ce7845571341634c21ea3f
hifanx <373441770@qq.com>
2025-10-17 04:51:18 +0800

    feat(nvim): add some components and adjust styles, lualine

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,7 +1,10 @@
 return {
   'nvim-lualine/lualine.nvim',
   event = { 'BufReadPre', 'BufNewFile' },
-  dependencies = { 'nvim-mini/mini.icons', 'AndreM222/copilot-lualine' },
+  dependencies = {
+    'nvim-mini/mini.icons',
+    'AndreM222/copilot-lualine',
+  },
   config = function()
     -- ╭──────────────────────────────────────────────────────────╮
     -- │ ⬇️ themes                                                │
@@ -49,6 +52,39 @@ return {
     local lazy = function() return lazy_status.updates() end
 
     -- ╭──────────────────────────────────────────────────────────╮
+    -- │ ⬇️ Mixed indent component                                │
+    -- ╰──────────────────────────────────────────────────────────╯
+    local indent = function()
+      local space_pat = [[\v^ +]]
+      local tab_pat = [[\v^\t+]]
+      local space_indent = vim.fn.search(space_pat, 'nwc')
+      local tab_indent = vim.fn.search(tab_pat, 'nwc')
+      local mixed = (space_indent > 0 and tab_indent > 0)
+      local mixed_same_line
+      if not mixed then
+        mixed_same_line = vim.fn.search([[\v^(\t+ | +\t)]], 'nwc')
+        mixed = mixed_same_line > 0
+      end
+      if not mixed then return '' end
+      if mixed_same_line ~= nil and mixed_same_line > 0 then return 'MI:' .. mixed_same_line end
+      local space_indent_cnt = vim.fn.searchcount({ pattern = space_pat, max_count = 1e3 }).total
+      local tab_indent_cnt = vim.fn.searchcount({ pattern = tab_pat, max_count = 1e3 }).total
+      if space_indent_cnt > tab_indent_cnt then
+        return 'MI:' .. tab_indent
+      else
+        return 'MI:' .. space_indent
+      end
+    end
+
+    -- ╭──────────────────────────────────────────────────────────╮
+    -- │ ⬇️ Trailing white spaces component                       │
+    -- ╰──────────────────────────────────────────────────────────╯
+    local trailing = function()
+      local space = vim.fn.search([[\s\+$]], 'nwc')
+      return space ~= 0 and 'TW:' .. space or ''
+    end
+
+    -- ╭──────────────────────────────────────────────────────────╮
     -- │ ⬇️ conditions for components to show                     │
     -- ╰──────────────────────────────────────────────────────────╯
     local conditions = {
@@ -70,16 +106,10 @@ return {
       },
       sections = {
         lualine_a = {
-          {
-            'mode',
-            icon = '',
-          },
+          { 'mode', icon = '' },
         },
         lualine_b = {
-          {
-            'branch',
-            icon = { ' ' },
-          },
+          { 'branch', icon = { ' ' } },
         },
         lualine_c = {
           {
@@ -96,6 +126,8 @@ return {
               removed = { fg = c.red },
             },
           },
+          { trailing },
+          { indent },
           { function() return '%=' end },
           {
             'diagnostics',
@@ -115,11 +147,7 @@ return {
           },
         },
         lualine_x = {
-          {
-            lazy,
-            cond = conditions.lazy_status,
-            color = { fg = c.peach },
-          },
+          { lazy, cond = conditions.lazy_status, color = { fg = c.peach } },
           {
             'copilot',
             cond = conditions.is_sif,
@@ -194,9 +222,7 @@ return {
           },
         },
         lualine_z = {
-          {
-            'progress',
-          },
+          { 'progress' },
         },
       },
     }
