1c14d43d7e455329b0c036810d82cd0833077505
hifanx <373441770@qq.com>
2025-10-18 20:54:30 +0800

    feat(nvim): add oil, update package list

---
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,37 @@
+return {
+  'stevearc/oil.nvim',
+  lazy = false,
+  dependencies = {
+    'benomahony/oil-git.nvim',
+  },
+  init = function() vim.keymap.set('n', '<leader>o', ':Oil<CR>', { desc = '[O]il' }) end,
+  config = function()
+    require('oil').setup {
+      default_file_explorer = false,
+      view_options = {
+        show_hidden = true,
+      },
+      use_default_keymaps = true,
+      -- See :help oil-actions for a list of all available actions
+      -- NOTE: these are defaults.
+      keymaps = {
+        ['g?'] = { 'actions.show_help', mode = 'n' },
+        ['<CR>'] = 'actions.select',
+        ['<C-s>'] = { 'actions.select', opts = { vertical = true } },
+        ['<C-h>'] = { 'actions.select', opts = { horizontal = true } },
+        ['<C-t>'] = { 'actions.select', opts = { tab = true } },
+        ['<C-p>'] = 'actions.preview',
+        ['<C-c>'] = { 'actions.close', mode = 'n' },
+        ['<C-l>'] = 'actions.refresh',
+        ['-'] = { 'actions.parent', mode = 'n' },
+        ['_'] = { 'actions.open_cwd', mode = 'n' },
+        ['`'] = { 'actions.cd', mode = 'n' },
+        ['~'] = { 'actions.cd', opts = { scope = 'tab' }, mode = 'n' },
+        ['gs'] = { 'actions.change_sort', mode = 'n' },
+        ['gx'] = 'actions.open_external',
+        ['g.'] = { 'actions.toggle_hidden', mode = 'n' },
+        ['g\\'] = { 'actions.toggle_trash', mode = 'n' },
+      },
+    }
+  end,
+}
