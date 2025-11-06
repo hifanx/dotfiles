4bbbf6330abd2a663f1db335a163289e46c64164
hifanx <373441770@qq.com>
2025-11-07 01:48:59 +0800

    feat(nvim): juicy autocmds

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -261,6 +261,40 @@ vim.api.nvim_create_autocmd('FileType', {
   callback = function() vim.cmd('setlocal formatoptions-=c formatoptions-=o') end,
 })
 
+vim.api.nvim_create_autocmd('BufReadPost', {
+  desc = 'Restore cursor to last edit position',
+  group = vim.api.nvim_create_augroup('restore_cursor_position', { clear = true }),
+  callback = function(ev)
+    local mark = vim.api.nvim_buf_get_mark(ev.buf, '"')
+    local lcount = vim.api.nvim_buf_line_count(ev.buf)
+    if mark[1] > 0 and mark[1] <= lcount then pcall(vim.api.nvim_win_set_cursor, 0, mark) end
+  end,
+})
+
+vim.api.nvim_create_autocmd('VimResized', {
+  desc = 'Auto resize splits on terminal resize',
+  command = 'wincmd =',
+})
+
+vim.api.nvim_create_autocmd('BufRead', {
+  desc = 'Syntax highlight for env files',
+  group = vim.api.nvim_create_augroup('dotenv', { clear = true }),
+  pattern = { '.env', '.env.*', '.secrets' },
+  callback = function() vim.bo.filetype = 'dosini' end,
+})
+
+vim.api.nvim_create_autocmd({ 'BufEnter', 'WinEnter' }, {
+  desc = 'Show cursorline only in active window',
+  group = vim.api.nvim_create_augroup('active_cursorline', { clear = true }),
+  callback = function() vim.opt_local.cursorline = true end,
+})
+
+vim.api.nvim_create_autocmd({ 'BufLeave', 'WinLeave' }, {
+  desc = 'Hide cursorline in inactive window',
+  group = 'active_cursorline',
+  callback = function() vim.opt_local.cursorline = false end,
+})
+
 -- }}}
 
 -- ╭──────────────────────────────────────────────────────────╮
