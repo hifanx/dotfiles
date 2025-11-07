2805db5427c7afdc973c29a744f3083f4c47f198
hifanx <373441770@qq.com>
2025-11-08 05:16:21 +0800

    chore(nvim): remove redundant pcall

--- nvim/.config/nvim/lua/plugins/pangu.lua
+++ nvim/.config/nvim/lua/plugins/pangu.lua
@@ -4,9 +4,9 @@ return {
   init = function()
     vim.api.nvim_create_autocmd('BufWritePre', {
       pattern = { '*.markdown', '*.md', '*.text', '*.txt', '*.wiki', '*.cnx' },
-      desc = 'Auto format text files with Pangu if available',
+      desc = 'Auto format text files with Pangu',
       callback = function()
-        pcall(function() vim.cmd('PanguAll') end)
+        vim.cmd('PanguAll')
         vim.notify('Format with Pangu', vim.log.levels.INFO)
       end,
     })
