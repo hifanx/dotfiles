23369d28e24c3e7b823b9c5881993bbd7f725de0
hifanx <373441770@qq.com>
2026-09-09 18:30:04 +0800

    chore(lofn): add neovim upgrade script for slackware

---
+++ lofn/upgrade_neovim.sh
@@ -0,0 +1,16 @@
+mkdir -p /tmp/nvimbuild/usr/local
+cd /tmp/nvimbuild
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+tar xzf nvim-linux-x86_64.tar.gz -C usr/local --strip-components=1
+rm nvim-linux-x86_64.tar.gz
+
+VER=$(usr/local/bin/nvim --version | head -1 | awk '{print $2}' | tr -d v)
+
+# optional post-install step baked into the package: make `vim` run nvim too
+mkdir -p install
+cat >install/doinst.sh <<'EOF'
+( cd usr/local/bin && ln -sf nvim vim )
+EOF
+
+makepkg -l y -c n /boot/extra/neovim-${VER}-x86_64-1local.txz
+installpkg /boot/extra/neovim-${VER}-x86_64-1local.txz # installs now, no reboot needed
